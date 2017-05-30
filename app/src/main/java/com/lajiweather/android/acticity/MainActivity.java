6811232bead39ea248969e05a45ae7c5c9e8bd6b
Sara <sara@gmail.com>
2017-05-30 15:05:58 +0800

    app/java/com.lajiweather.android/db

--- app/src/main/java/com/lajiweather/android/acticity/MainActivity.java
+++ app/src/main/java/com/lajiweather/android/acticity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.lajiweather.android;
+package com.lajiweather.android.acticity;
 
 import android.app.Activity;
 import android.os.Bundle;
+
+import com.lajiweather.android.R;
 
 public class MainActivity extends Activity {
 
