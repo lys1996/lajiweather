c635c3c4c6fbc1f411b7642e61d770621d699396
Sara <sara@gmail.com>
2017-05-30 22:59:20 +0800

    完成遍历省市县三级列表的功能“ git commit -m完成遍历省市县三级列表的功能“

--- app/src/main/java/com/lajiweather/android/acticity/MainActivity.java
+++ app/src/main/java/com/lajiweather/android/acticity/MainActivity.java
@@ -1,11 +1,13 @@
 package com.lajiweather.android.acticity;
 
 import android.app.Activity;
+import android.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 
 import com.lajiweather.android.R;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
